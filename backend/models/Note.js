b5393ac89651d11b2c7c90010ea36157ab3731f8
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-18 19:51:49 +0530

    created both schemas

---
+++ backend/models/Note.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const NoteSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3
+    },
+    description: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tags: {
+        type: String,
+        default: "General"
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.models.Note || mongoose.model("Note", NoteSchema);
---
+++ backend/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.models.User || mongoose.model("User", UserSchema);
