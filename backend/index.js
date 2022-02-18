45c4b791402619beb25c13e150ea7761863e1550
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-18 19:11:25 +0530

    ceating routes

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const cors = require('cors');
 
 const app = express();
 
-app.use();
+app.use(cors);
+app.use(express.json());
+
+// Available Routes
+app.use("./api/auth",require("./routes/auth"));
+app.use("./api/notes",require("./routes/notes"));
+
+
+app.listen(5000, () => console.log("App is Listening at http://localhost:5000"));
