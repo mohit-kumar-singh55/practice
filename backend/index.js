9990863216fd5033f98af864fa13d389d152f7b6
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-19 14:22:12 +0530

    TypeError: Router.use() requires a middleware

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,8 @@ app.use(cors);
 app.use(express.json());
 
 // Available Routes
-app.use("./api/auth", require("./routes/auth"));
-app.use("./api/notes", require("./routes/notes"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 
-app.listen(5000, () => console.log("App is Listening at http://localhost:5000"));
+app.listen(3000, () => console.log("App is Listening at http://localhost:3000"));
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,28 @@
 const express = require("express");
 // const { body, validationResult } = require("express-validator");    // anothor package
+const User = require("../models/User")
 
 const router = express.Router();
 
 
 
 // SignUp - createauser POST 
-router.post('/createuser', async (req,res)=>{
-    const {name,email,password} = req.body;
+router.post('/createuser', async (req, res) => {
+    const { name, email, password } = req.body;
+
+    try {
+        // check if user already exists
+        let user = await User.findOne({ email: email });
+        if (user) res.status(400).json({ error: "Sorry, User with this email already exists!" });
+
+        // create user
+        user = User.create({ name, email, password });
+        res.status(200).json({ user });
+    }
+    catch (error) {
+        res.status(500).json({ error });
+    }
 })
+
+
+module.exports = router;
