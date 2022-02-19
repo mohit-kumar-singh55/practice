29f3a120d11c38ab8b2393405ebcdab11f1f692f
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-19 17:28:01 +0530

    env is not working

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 const connection = {};
 
+const MONGO_URI = 'mongodb://localhost:27017/practice?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
+
 async function dbConnect() {
     if (connection.isConnected) return;
 
-    const db = await mongoose.connect(process.env.MONGO_URI, {
+    const db = await mongoose.connect(MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
